4f78e2d8dd64af490b053faa7e04a6045b4c58ff
Shashank saxena <shashank.kumar@appinventiv.com>
2024-03-31 22:14:37 +0530

    delete from begning and insert at starting program add successfully

---
+++ src/enum/MENU.ts
@@ -0,0 +1,6 @@
+export enum MENU {
+    INSERT_ELEMENT_BEGINNING = "Insert element at beginning",
+    TRAVERSE_ALL_ELEMENT = "Traverse All the element",
+    INSERT_ELEMENT_END = "Insert element at end",
+    DELETE_ELEMENT_BEGINNING = "Delete element from beginning"
+}
---
+++ src/enum/response.messages.ts
@@ -0,0 +1,6 @@
+export enum RESPONSE_MESSAGES {
+    ELEMENT_INSERT_SUCCESS = "Element insert successfully",
+    NO_ELEMENT_AVAILABLE = "No element available to delete",
+    ELEMENT_DELETE = "Element delete successfully",
+    ENTER_VALID_VALUE = "Enter the valid value"
+}
